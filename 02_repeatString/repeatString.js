24e1b71fc82790f79b42257da80d8dd21f66ec3c
lippyyy <111197353+lippyyy@users.noreply.github.com>
2024-01-01 15:53:56 +0000

    Return string multiple times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedString = "";
 
+    while (num > 0) {
+        repeatedString += string
+        num--
+    }
+
+    if (num < 0) {
+        return "ERROR"
+    }
+
+    return repeatedString
 };
 
 // Do not edit below this line
