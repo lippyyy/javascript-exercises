1acce7303b2929ca853799cd54ac4babba1b11d2
lippyyy <111197353+lippyyy@users.noreply.github.com>
2024-01-01 18:21:44 +0000

    Convert celcius to farenheit and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(num) {
+  convertedNum = ((5/9) * (num - 32))
+  return Math.round(convertedNum * 10) / 10
+}
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(num) {
+  convertedNum = ((num * (9/5)) + 32)
+  return Math.round(convertedNum * 10) / 10
+}
 
 // Do not edit below this line
 module.exports = {
