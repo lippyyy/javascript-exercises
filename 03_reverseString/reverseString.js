70d3c6ec664eea055707b790f49b76dc35edb039
lippyyy <111197353+lippyyy@users.noreply.github.com>
2024-01-01 16:02:03 +0000

    Reverse string input

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = "";
 
+    for (let i = string.length; i > 0; i--) {
+        reversedString += string[i-1]
+    }
+
+    return reversedString
 };
 
 // Do not edit below this line
