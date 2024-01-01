4fc100e7cbde6c3abc27a8421eeedd1ffca42d6e
lippyyy <111197353+lippyyy@users.noreply.github.com>
2024-01-01 17:24:27 +0000

    Sum numbers between 2 intervals

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
+    if (numOne < 0 || numTwo < 0) {
+        return "ERROR"
+    } if (typeof numOne !== 'number' || typeof numTwo !== 'number') {
+        return "ERROR"
+    } else if (numOne < numTwo) {
+        while (numOne <= numTwo) {
+            sum += numOne
+            numOne++
+        }
+        return sum
+    } else if (numOne > numTwo) {
+        while (numOne >= numTwo) {
+            sum += numTwo
+            numTwo++
+        }
+        return sum
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
